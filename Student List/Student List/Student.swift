bb8b640457309ea5da026320c39de995ff73ad1e
Chris Vombaur <89802742+Cvombaur18@users.noreply.github.com>
2021-11-16 15:45:08 -0700

    Add files via upload

---
+++ Student List/Student List/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  Student List
+//
+//  Created by chris vombaur on 11/5/21.
+//
+
+import Foundation
+
+class Student {
+    
+    init(firstName: String, lastName: String, age: Int)
+    
+    self.firstName = firstN
+}
